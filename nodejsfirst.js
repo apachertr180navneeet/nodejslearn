154bc86f3bfd2bb1c54848d5bc09a5398e45020a
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-11-24 12:05:24 +0545

    error and warning

--- nodejsfirst.js
+++ nodejsfirst.js
@@ -1,1 +1,11 @@
-console.log('Hello Navneet ');
+//console.log('Hello Navneet');
+
+
+//console.log('Hello %s', 'JavaTpoint');   
+
+
+//console.error(new Error('Hell! This is a wrong method.'));  
+
+
+const name = 'Navneet';  
+console.warn(`Don't mess with me ${name}! Don't mess with me!`);  
